2795573d7e77be7fdcc38102d782aca1053ce489
May Tusek <tusek.may@gmail.com>
2023-05-05 09:56:36 -0700

    Adds theme provider.

--- projects/app/src/main.tsx
+++ projects/app/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Theme, ThemeProvider } from '@emotion/react'
+
+const theme: Theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme} >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
