4e6fbaac6adbac390f5dd17c8737338d180cf4cd
May Tusek <tusek.may@gmail.com>
2023-05-05 10:21:53 -0700

    Changes a bunch of stuff.

--- projects/app/src/main.tsx
+++ projects/app/src/main.tsx
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Theme, ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
 
 const theme: Theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
-      <CssBaseline>
-        <App />
-      </CssBaseline>
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 )
