9c28b900356cc8e8b5c6538cc2eecb205aa37afc
May Tusek <tusek.may@gmail.com>
2023-05-06 06:29:08 -0700

    Empty theme for now.

--- projects/app/src/themes/dark.theme.ts
+++ projects/app/src/themes/dark.theme.ts
@@ -1,11 +1,4 @@
 import { createTheme } from "@mui/material"
 
 export const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    background: {
-      default: "#121212",
-      paper: "#1E1E1E",
-    }
-  },
 })
