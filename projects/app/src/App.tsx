1737dc201b2ea09938f3dbe5669d2361d5b4f9d2
May Tusek <tusek.may@gmail.com>
2023-10-21 13:05:53 -0700

    Updates app structure.

--- projects/app/src/App.tsx
+++ projects/app/src/App.tsx
@@ -1,7 +1,8 @@
+import { CssBaseline } from "@mui/material"
 import "./index.css"
 
 import React from "react"
 
 export const App: React.FC = () => {
-  return <></>
+  return <CssBaseline />
 }
--- projects/app/src/main.tsx
+++ projects/app/src/main.tsx
@@ -1,16 +1,10 @@
-import { ThemeProvider } from "@emotion/react"
-import { CssBaseline } from "@mui/material"
 import React from "react"
 import ReactDOM from "react-dom/client"
 
 import { App } from "./App.tsx"
-import { theme } from "./themes/theme.ts"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
